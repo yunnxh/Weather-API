fc4590429395ab4aa2c220666a7fc6c451d1adc5
Yunish Maharjan <127837145+yunnxh@users.noreply.github.com>
2023-07-16 15:36:59 +0545

    script.js

--- weather/script.js
+++ weather/script.js
@@ -1,20 +1,17 @@
-// Function to retrieve weather data for a specific city
 function getWeather(city) {
-    const apiKey = 'c20925545257cec1439f5bfcf3f30ea9'; // Replace with your OpenWeatherMap API key
+    const apiKey = 'c20925545257cec1439f5bfcf3f30ea9';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
-    // Make a GET request to the API
+
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        // Extract the necessary weather information from the API response
+
         const weather = {
           temperature: data.main.temp,
           windSpeed: data.wind.speed,
           humidity: data.main.humidity
         };
-  
-        // Display the current weather information in the HTML elements
+ 
         const cityHeading = document.querySelector('.current-weather h2');
         const temperature = document.querySelector('.current-weather h6:nth-of-type(1)');
         const wind = document.querySelector('.current-weather h6:nth-of-type(2)');
@@ -24,17 +21,15 @@ function getWeather(city) {
         temperature.textContent = `Temperature: ${weather.temperature}°C`;
         wind.textContent = `Wind: ${weather.windSpeed} M/S`;
         humidity.textContent = `Humidity: ${weather.humidity}%`;
-  
-        // Fetch the 5-day forecast for the city
+
         const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
         return fetch(forecastApiUrl);
       })
       .then(response => response.json())
       .then(forecastData => {
-        // Extract the necessary forecast information from the API response
-        const forecastItems = forecastData.list.slice(0, 5); // Get the first 5 forecast items
-  
-        // Display the 5-day forecast information in the HTML elements
+
+        const forecastItems = forecastData.list.slice(0, 5); 
+
         const weatherCards = document.querySelector('.weather-cards');
         weatherCards.innerHTML = '';
   
@@ -51,25 +46,22 @@ function getWeather(city) {
         });
       })
       .catch(error => {
-        // Display an error message if there's an issue with the API request
+
         console.log('Error:', error);
       });
   }
-  
-  // Function to handle the search button click
+
   function handleSearch() {
     const cityInput = document.querySelector('.city-input');
     const city = cityInput.value;
     getWeather(city);
   }
-  
-  // Load the weather data for the assigned city when the page loads
+
   document.addEventListener('DOMContentLoaded', function() {
-    const assignedCity = 'Ozark'; // Replace with your assigned city
+    const assignedCity = 'Ozark'; 
     getWeather(assignedCity);
   });
-  
-  // Add event listener to the search button
+
   const searchButton = document.querySelector('.search-btn');
   searchButton.addEventListener('click', handleSearch);
   
